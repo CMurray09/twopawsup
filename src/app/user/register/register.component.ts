2cee70d85769235a470672f7686c00c589aaa32e
Cameron M <cameronmurray@hotmail.ca>
2022-07-01 23:46:20 +0100

    Validation and form functionality

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(150)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$')
+    ]),
+    confirm_password: new FormControl('', [
+      Validators.required
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(11),
+      Validators.maxLength(11)
+    ])
+  });
 }
+
+
