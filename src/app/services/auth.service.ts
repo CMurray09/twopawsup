1202d7d10e48c9a48d6b258ae9b1f1f7fef9c089
Cameron M <cameronmurray@hotmail.ca>
2022-07-05 20:10:39 +0100

    Logout refactored to service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat
 import IUser from "src/app/models/user.model";
 import {Observable} from "rxjs";
 import {map, delay} from 'rxjs/operators';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore, private router: Router) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
@@ -42,5 +43,14 @@ export class AuthService {
     });
 
     await userCred.user.updateProfile({ displayName: userData.name })
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+
+    await this.router.navigateByUrl('/');
   }
 }
