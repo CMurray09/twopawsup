1eb83c86801ee4ec69d4fb2e54cd9262ae67b28a
Cameron M <cameronmurray@hotmail.ca>
2022-07-07 13:29:31 +0100

    Video dropped checks

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
   isDragover: boolean = false;
+  file: File | null = null;
+  isVideoDropped: boolean = false;
 
   constructor() { }
 
@@ -15,6 +17,12 @@ export class UploadComponent implements OnInit {
 
   storeFile($event: Event) {
     this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    this.isVideoDropped = true;
+    console.log(this.file);
   }
 
 }
