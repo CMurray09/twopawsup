ea9944ac50b36ee2b06b4c40ce82fb6efc06b536
Cameron M <cameronmurray@hotmail.ca>
2022-07-07 20:13:07 +0100

    Edit component

---
+++ src/app/video/edit/edit.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+import IClip from "src/app/models/clip.model";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('', {
+    nonNullable: true
+  });
+
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  });
+
+  editForm: FormGroup = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  })
+
+  constructor(private modal: ModalService) { }
+
+  ngOnInit(): void {
+    this.modal.register('editClip');
+  }
+
+  ngOnDestroy(): void {
+    this.modal.unregister('editClip');
+  }
+
+  ngOnChanges(): void {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.clipID.setValue(this.activeClip.docID!);
+    this.title.setValue(this.activeClip.title);
+  }
+}
