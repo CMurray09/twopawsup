137a17441354fb091459d02da48c05314ec0c259
Cameron M <cameronmurray@hotmail.ca>
2022-07-08 22:40:45 +0100

    Sort by timestamp

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -8,7 +8,7 @@ import {
 import IClip from "src/app/models/clip.model";
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {switchMap, map} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {of, BehaviorSubject, combineLatest} from 'rxjs';
 import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Injectable({
@@ -27,16 +27,20 @@ export class ClipService {
     return this.clipsCollection.add(data);
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap(user => {
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([
+      this.auth.user,
+      sort$
+    ]).pipe(
+      switchMap(values => {
+        const [user, sort] = values;
         if (!user) {
           return of([])
         }
 
         const query = this.clipsCollection.ref.where(
           'uid', '==', user.uid
-        )
+        ).orderBy('timestamp', sort === '1' ? 'desc' : 'asc');
 
         return query.get();
       }),
